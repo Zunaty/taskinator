85fea536795aead6bc1dea1fe21d961227a6a450
Victor Perez <zunaty@gmail.com>
2021-07-09 13:35:48 -0600

    updated form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
-var buttonE1 = document.querySelector("#save-task")
-console.log(buttonE1)
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-}
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
