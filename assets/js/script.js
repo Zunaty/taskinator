7b5d565e5646e3f35f31b7343d6c02e26fdce86a
Victor Perez <zunaty@gmail.com>
2021-07-09 14:37:32 -0600

    list items show type

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,24 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
 var createTaskHandler = function(event) { 
-  event.preventDefault(); 
+    event.preventDefault(); 
 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+    // holding the info for info and info type
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // creating a list item
+    var listItemEl = document.createElement("li"); 
+    listItemEl.className = "task-item";
+
+    // creating div item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.classname = "task-info";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+    tasksToDoEl.appendChild(listItemEl); 
+    }; 
 
 formEl.addEventListener("submit", createTaskHandler);
